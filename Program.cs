796ebca0fc244a677fd662ff9ad1439b0b90cb5d
JonPiniesEcheguren <jon.pinies.ech@gmail.com>
2023-05-15 09:57:40 +0200

    Ejercicio mejorado

--- Program.cs
+++ Program.cs
@@ -3,9 +3,19 @@
 Clear();
 Write("Ingrese el nombre del país (México/Japón/España):");
 string? nombre = Console.ReadLine();
+if (nombre == null)
+    {
+        Write("País inválido");
+        return;
+    }
 
 Write("Ingrese la capital del país:");
 string? capital = Console.ReadLine();
+if (nombre == null)
+    {
+        Write("Capital de país inválido");
+        return;
+    }
 
 Pais? pais = null;
 
@@ -14,16 +24,31 @@ switch (nombre)
     case "México":
         Write("Ingrese el idioma del país:");
         string? idioma = ReadLine();
+        if (idioma == null)
+    {
+        Write("Idioma inválido");
+        return;
+    }
         pais = new Mexico(nombre!, capital!, idioma!);
         break;
     case "Japón":
         Write("Ingrese moneda del país:");
         string? moneda = ReadLine();
+        if (moneda == null)
+    {
+        Write("Moneda inválida");
+        return;
+    }
         pais = new Japon(nombre!, capital!, moneda!);
         break;
     case "España":
         Write("Ingrese region del país:");
         string? region = ReadLine();
+        if (region == null)
+    {
+        Write("Región invalida");
+        return;
+    }
         pais = new Espana(nombre!, capital!, region!);
         break;
     default:
